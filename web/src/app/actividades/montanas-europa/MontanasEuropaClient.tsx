390816df54a29666f16333e29d0f4997726db0eb
Dabit <davidlpena74@gmail.com>
2026-02-19 07:14:15 +0100

    Adjust: Shift Europe Mountains map down by 20% - Bump to 3.4.9

--- web/src/app/actividades/montanas-europa/MontanasEuropaClient.tsx
+++ web/src/app/actividades/montanas-europa/MontanasEuropaClient.tsx
@@ -27,7 +27,7 @@ export default function MontanasEuropaClient() {
                 backgroundPaths={EUROPE_PATHS}
                 viewBox="0 0 800 600"
                 initialZoom={1.65}
-                initialPan={{ x: -160, y: -230 }}
+                initialPan={{ x: -160, y: -110 }}
                 elevationHeight={2}
                 taskId={taskId}
                 colorTheme="emerald"
